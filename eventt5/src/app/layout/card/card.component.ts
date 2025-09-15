bf8afc69110b3e698a9dc8be108c0e79e485e391
Miles Larkot <mileslarkot@proton.me>
2025-09-15 19:18:18 +0100

    styling
    added not-found page and made card component reusable

--- eventt5/src/app/layout/card/card.component.ts
+++ eventt5/src/app/layout/card/card.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
 })
 export class CardComponent {
-
+  @Input() imageUrl: string = '';
+  @Input() buttonStyle: string = 'bg-blue-500 text-white hover:bg-blue-600';
 }
